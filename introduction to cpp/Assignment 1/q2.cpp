1e432c6b7fd4dba4fa99a66c6189f03893f36003
pri2405 <psharma532003@gmail.com>
2023-09-18 14:54:05 +0530

    move files to assignment1

---
+++ introduction to cpp/Assignment 1/q2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//print 10 using two +ve no. less than 6
+
+int main(){
+
+    int a=5;
+    cout<<a+a<<endl;
+
+    return 0;
+
+}
---
+++ introduction to cpp/Assignment 1/q5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//program to find the circumference of the circle with radius  10
+
+int main(){
+
+    float radius = 10;
+    float circumference = 2*3.14*radius ;
+
+    cout<<circumference<<endl;
+
+    return 0;
+}
